4f786f048c4c6726f2f785155aadffe003b8855b
MahaSabry5 <maha08526@gmail.com>
2021-12-16 14:35:13 +0200

    mapsController

---
+++ app/Controllers/MapsController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+class MapsController{
+    public function __construct(){
+        print 'Welcome from Maps Controller';
+
+    }
+
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require __DIR__.'/vendor/autoload.php';
 
 use App\Controllers\AboutController;
 use App\Controllers\HomeController;
+use App\Controllers\MapsController;
 
 $homeController = new HomeController();
 $aboutController = new AboutController();
+$mapsController = new MapsController();
