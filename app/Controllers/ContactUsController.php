378a9987c14b260a86e24707fd4ffbe8638cca84
MahaSabry5 <maha08526@gmail.com>
2021-12-16 14:31:37 +0200

    contactUsController

---
+++ app/Controllers/ContactUsController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+class ContactUsController{
+    public function __construct(){
+        print 'Welcome from Contact Us Controller';
+
+    }
+
+}
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 require __DIR__.'/vendor/autoload.php';
 
 use App\Controllers\AboutController;
+use App\Controllers\ContactUsController;
 use App\Controllers\HomeController;
 
 $homeController = new HomeController();
 $aboutController = new AboutController();
+$contactUsController = new ContactUsController();
